78db109b92a688deb6b08177dc67b759bd6a5d2d
unknown <safavimoghadam.89@gmail.com>
2021-03-04 23:21:16 +0330

    use Router in first page for switch between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import logo from './logo.svg';
+import Login from './login/Login';
 
-import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">Wellcome Page</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/login'} className="nav-link">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Switch>
+              <Route path='/login' component={ Login } />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
+/*
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 
 class App extends Component {
   state = {
@@ -43,7 +84,6 @@ render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -75,3 +115,4 @@ render() {
 }
 
 export default App;
+*/
