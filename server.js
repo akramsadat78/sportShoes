85fe7c88749cd5111289ef6b32583c5aad96407e
unknown <safavimoghadam.89@gmail.com>
2021-03-04 18:13:59 +0330

    connect to db

--- server.js
+++ server.js
@@ -2,21 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-/*add*/
-//const database = require('./src/database/mongo');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/*add*/
+const mongoose = require('mongoose');
+const config = require('./file/database/mongo');
+const shoeRoute = require('./file/operation/route');
+
+mongoose.connect(config.DB, {
+  useNewUrlParser: true,
+  useFindAndModify:false,
+  useCreateIndex:true,
+  useUnifiedTopology:true
+}).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+app.use('/shoe', shoeRoute);
+/*
+startDatabase().then(async () => {
+  console.log("startDatabase")
+}
+);
+
+add().then(async () => {
+  console.log("add")
+}
+);*/
+
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
-//database();
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
@@ -24,6 +46,8 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
