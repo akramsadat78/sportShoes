226b63162258d53524602c085028328c56fc6e11
unknown <safavimoghadam.89@gmail.com>
2021-03-09 14:25:51 +0330

    change path name for login(LoginComponent)

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Login from './login/Login';
+import Login from './login/LoginComponent';
 
 class App extends Component {
   render() {
