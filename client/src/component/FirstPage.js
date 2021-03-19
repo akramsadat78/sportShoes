100e0058e72c017f5c4537a6da0dba6103a07de6
unknown <safavimoghadam.89@gmail.com>
2021-03-19 23:08:19 +0330

    create FirstPage component

---
+++ client/src/component/FirstPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/FirstPage.css';
+
+export default class FirstPage extends Component {
+
+  render() {
+    return (
+        <body>
+      <p>wellcoma</p>
+      </body>
+    );
+  }
+}
