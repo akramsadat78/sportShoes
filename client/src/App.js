9766aea11e59a8b85ee3e89b80cb43967c971765
unknown <safavimoghadam.89@gmail.com>
2021-03-19 23:04:17 +0330

    use Router for navigate between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import LoginComponent from './component/LoginComponent';
+//import GetInformation from './component/GetInfomation';
+//import SignUpComponent from './component/SignUpComponent'
+import FirstPage from './component/FirstPage';
 
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App () {
-  return ( <LoginComponent/> );
+ return ( 
+  <Router>
+    <Route exact path = { `/` } component = {LoginComponent}/>
+    <Route path = { `/first` } component = {FirstPage}/>
+  </Router>
+ );
 }
 
 export default App;
