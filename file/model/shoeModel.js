db4c6a2496328b93f3e0c73197993cf2c872da3d
unknown <safavimoghadam.89@gmail.com>
2021-03-25 23:55:57 +0430

    create shoeModel and shoeRout for shoes

---
+++ file/model/shoeModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for shoe
+let Shoe = new Schema({
+  shoe_name: {
+    type: String
+  },
+  shoe_model: {
+    type: String
+  },
+  shoe_code: {
+    type: Number
+  },
+  shoe_color: {
+    type: String
+  },
+  shoe_size: {
+    type: Number
+  },
+  shoe_count: {
+    type: Number
+  },
+  shoe_purchase_date: {
+    type: Date
+  },
+  shoe_sale_date: {
+    type: Date
+  },
+  shoe_cost_buy: {
+    type: Number
+  },
+  shoe_cost_sale: {
+    type: Number
+  },
+  shoe_profit: {
+    type: Number
+  },
+  shoe_image: {
+    type: String
+  },
+  shoe_description: {
+    type: String
+  }
+},{
+    collection: 'shoe'
+});
+
+module.exports = mongoose.model('shoe', Shoe);
---
+++ file/operation/shoeRout.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const shoeRoutes = express.Router();
+
+let Shoe = require('../model/shoeModel');
+
+// create a new doqument
+shoeRoutes.route('/add').post(function (req, res) {
+  console.log("+++++++++++++++++++++++++ add new shoe +++++++++++++++++++++++++",req.body) 
+  let shoe = new Shoe(req.body);
+  console.log(shoe)
+  shoe.save()
+        .then(() => res.json({
+            message: "Created account successfully"
+        }))
+        .catch(err => res.status(400).json({
+            "error": err,
+            "message": "Error creating account"
+        })) 
+});
+
+//get information
+shoeRoutes.route('/').get(function (req, res) {
+    console.log("+++++++++++++++++++++++++ get information +++++++++++++++++++++++++") 
+    Shoe.find(function(err, shoes){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(shoes);
+    }
+  });
+});
+
+// Defined edit route
+shoeRoutes.route('/edit/:id').get(function (req, res) {
+  let id = req.params.id;
+  Shoe.findById(id, function (err, shoe){
+      res.json(shoe);
+  });
+});
+
+//  Defined update route
+shoeRoutes.route('/update/:id').post(function (req, res) {
+    Shoe.findById(req.params.id, function(err, shoes) {
+    if (!shoes)
+      res.status(404).send("data is not found");
+    else {
+        shoe.shoe_name = req.body.shoe_name;
+        shoe.shoe_code = req.body.shoe_code;
+
+        shoe.save().then(shoe => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+shoeRoutes.route('/delete/:id').get(function (req, res) {
+    Shoe.findByIdAndRemove({_id: req.params.id}, function(err, shoe){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = shoeRoutes;
