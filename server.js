cc45ec7dde5a12976cfa57cefb9bd567eb60fa1a
unknown <safavimoghadam.89@gmail.com>
2021-03-25 23:57:50 +0430

    modify for adding shoeRout

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./file/database/mongo');
-const shoeRoute = require('./file/operation/route');
 const loginRoute = require('./file/operation/loginRout');
+const shoeRoute = require('./file/operation/shoeRout');
 const path = require('path');
 
 app.use(bodyParser.json());
@@ -29,6 +29,7 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.use('/login', loginRoute);
+app.use('/information', shoeRoute);
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
@@ -48,55 +49,3 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-/*const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./file/database/mongo');
-const shoeRoute = require('./file/operation/route');
-const loginRoute = require('./file/operation/loginRout');
-const path = require('path');
-
-mongoose.connect(config.DB, {
-  useNewUrlParser: true,
-  useFindAndModify:false,
-  useCreateIndex:true,
-  useUnifiedTopology:true
-}).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-console.log("hello server");
-app.use('/shoe', shoeRoute);
-
-app.get('/login', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.use('/login', loginRoute);
-
-
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
-app.listen(port, () => console.log(`Listening on port ${port}`));*/
