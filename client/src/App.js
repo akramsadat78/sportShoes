ea5271421ddccb936d69f6b3ae088eaa76327827
unknown <safavimoghadam.89@gmail.com>
2021-03-27 19:20:15 +0430

    clean code

--- client/src/App.js
+++ client/src/App.js
@@ -14,52 +14,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
 export default class App extends Component {
-//function App () {
+
   constructor(props){
     super(props);
     this.state = {
         array_code: Array.from(Array(1000).keys())
     }
-}
+  }
 
   render() {
- return ( 
-  <Router >
-    <Route exact path = { `/` } component = {LoginComponent}/>
-    <Route path = { `/first` } component = {FirstPage}/>
-    <Route path = { `/SubmitInformation` } component = {SubmitInformation}/>
-    <Route path = { `/Search` } component = {Search}/>
-    <Route path = { `/ProfitCalculation` } component = {ProfitCalculation}/>
-    
-    
-     {this.state.array_code.map((item, index) => ( 
-        <Route path = { `/shoe${item}` }
-        render={(props) => <Update {...props}  dataParentToChild = { item }
-            /*  component = {() => < Update dataParentToChild = { item }*//>}
-        />
-       ))
-     }
-   
-    {/*<Route path = { `/shoe4` } 
-                        component =  {() =>  <Update message="hello"/> }
- />
-    <Route path = { `/shoe4` } 
-                        component =  {() =>  <Update parentCallback = {this.handleCallback}/> }
-                  />
-                  {this.state.data}
-     {/* this.props.message.map((item, index) => ( 
-        <Router>
-          <Route path = { `/shoe4` } component = { Update } />
-        </Router>
-      )) 
-     */}
-    
-    
-  </Router>
-
- );
+    return ( 
+      <Router >
+        <Route exact path = { `/` } component = {LoginComponent}/>
+        <Route path = { `/first` } component = {FirstPage}/>
+        <Route path = { `/SubmitInformation` } component = {SubmitInformation}/>
+        <Route path = { `/Search` } component = {Search}/>
+        <Route path = { `/ProfitCalculation` } component = {ProfitCalculation}/>
+        {this.state.array_code.map((item, index) => ( 
+          <Route
+                path = { `/shoe${item}` }
+                render={(props) => <Update {...props}  dataParentToChild = { item }/>}
+          />
+         ))
+        }
+      </Router>
+    );
   }
 }
 
