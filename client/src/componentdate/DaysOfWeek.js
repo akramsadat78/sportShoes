09c7dd52c177b8ec61b9b56cbb497b9e3c7c68f5
unknown <safavimoghadam.89@gmail.com>
2021-04-13 20:14:14 +0430

    components for persian date

---
+++ client/src/componentdate/DaysOfWeek.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../css/basic.css';
+// Day of week names for use in date-picker heading
+const dayOfWeekNames = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج'];
+
+export default class DaysOfWeek extends Component {
+  static propTypes = {
+    styles: PropTypes.object
+  };
+
+  render() {
+    const { styles } = this.props;
+
+    return (
+     
+      <div id="daysOfWeek">
+         {/*<div className={styles.daysOfWeek}>*/}
+        { dayOfWeekNames.map((name, key) => <div key={key}>{name}</div>) }
+      </div>
+    );
+  }
+}
---
+++ client/src/componentdate/DaysViewHeading.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { persianNumber } from '../utils/persian';
+import { leftArrow, rightArrow } from '../utils/assets';
+import '../css/basic.css';
+
+export default class Heading extends Component {
+  static propTypes = {
+    month: PropTypes.object.isRequired
+  };
+
+  static contextTypes = {
+    styles: PropTypes.object,
+    nextMonth: PropTypes.func.isRequired,
+    prevMonth: PropTypes.func.isRequired,
+    setCalendarMode: PropTypes.func.isRequired
+  };
+
+  handleMonthClick(event) {
+    const { setCalendarMode } = this.context;
+    event.preventDefault();
+    setCalendarMode('monthSelector');
+  }
+
+  render() {
+    const { nextMonth, prevMonth } = this.context;
+    const { month, styles } = this.props;
+
+    return (
+      <div id="heading">
+      {/*<div className={styles.heading}>*/}
+        <button id="title" onClick={this.handleMonthClick.bind(this)}>
+        {/*<button className={styles.title} onClick={this.handleMonthClick.bind(this)}>*/}
+          { persianNumber(month.format('jMMMM jYYYY')) }
+        </button>
+        <button
+          type="button"
+          title="ماه قبل"
+          id="prev"
+          /*className={styles.prev}*/
+          onClick={prevMonth}
+          dangerouslySetInnerHTML={rightArrow}
+        />
+        <button
+          type="button"
+          title="ماه بعد"
+          id="next"
+          /*className={styles.next}*/
+          onClick={nextMonth}
+          dangerouslySetInnerHTML={leftArrow}
+        />
+      </div>
+    );
+  }
+}
