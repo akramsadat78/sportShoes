ecb0eea4f155e8d3855aafacb5dcec2eb075eca6
unknown <safavimoghadam.89@gmail.com>
2021-05-02 23:24:34 +0430

    correctdupliacate name and color and writw comment for it

--- client/src/component/Search.js
+++ client/src/component/Search.js
@@ -2,131 +2,37 @@ import React, { Component } from 'react';
 import '../css/Search.css';
 import DropdownSelect from './DropdownSelect';
 import DropdownSelectName from './DropdownSelectName';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {Link} from "react-router-dom";
 export default class Search extends Component {
   constructor(props){
     super(props);
 
-    this.onSubmit = this.handleSubmit.bind(this);
-
     this.state = {
-      shoe_name: '',
-      shoe_model: '',
-      shoe_code: '',
-      shoe_color: '',
-      shoe_size: '',
-      response: '',
-      responseToPost: '',
-      validation:0,
-      validationsize:0,
-      array:[],
-      array_code:[],
-      counter:0,
-      data: 'Data from parent',
-      arrayShowModel:[],
-      checked:1,
+      shoe_name: '',//shoe_name in DB
+      shoe_model: '',//shoe_model in DB
+      shoe_code: '',//shoe_code in DB
+      shoe_color: '',//shoe_color in DB
+      shoe_size: '',//shoe_size in DB
       arraymodel:['Nike','Pama','Adidas','Reebook','Skechers','Asics','Puma'],
-      detail:0,
-      arrayShoeName:[],
-      arrayShoeColor:[],
-      save_model:'',
-      save_name:'',
-      save_color:'',
-      save_size:'',
-      clickedModel: 1,
-      clickedName:1,
-      clickedColor:1,
-      clickedSize:1,
-      searchSize : '',
-      arraySize : [],
-      array_code_size : ''
+      arrayShoeName:[],//keep names of model that is  selected
+      arrayShoeColor:[],//keep colors of model and name that are selected
+      save_model:'',//model that user is selected
+      save_name:'',//name that user is selected
+      save_color:'',//color that user is selected
+      save_size:'',//size that user is selected 
+      array_image:[],//save image of search => model,name,color
+      array_code:[],//save code of search => model,name,color
+      array_size : [],//save image of search => size
+      array_code_size : '',//save code of search => size
+      validation:0,//check information (search model,name,color) is selected true or not (and check also  user does not search empty)
+      validationsize:0//check information (search size) is selected true or not (and check also  user does not search empty)
+     
     }
     
   }
 
-  handelSearchSize(event) {
-    this.setState({
-      searchSize: event.currentTarget.value
-    }); 
-  }
-
-  handleToggleColor(event,clickedColor) {
-    let value = event.currentTarget.value
-    fetch('/information', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-      .then(response => response.json())
-      .then(result => {
-
-        result.map(index => {
-            
-          if((index.shoe_model == this.state.save_model) ){
-            if( (index.shoe_name == this.state.save_name) ){
-              if( (index.shoe_color == value)){
-                index.shoe_size.map((value) => {
-                  this.setState({
-                    save_color:index.shoe_color,
-                    clickedColor: 0
-                  });
-                })
-              }
-            }
-          }
-          
-        })
-    })
-  }
-
-
-  handleToggleName(event,clickedName) {
-    let value = event.currentTarget.value
-    this.setState({
-      arrayShoeColor:[],
-      save_name:value,
-      save_color:'',
-      save_size:'',
-      clickedName:1,
-      clickedColor:1,
-      clickedSize:1
-    });
-
-    fetch('/information', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-      .then(response => response.json())
-      .then(result => {
-
-        result.map(index => {
-          if( (index.shoe_name == value) && (index.shoe_model == this.state.save_model) ){
-            if( clickedName == 1){
-              this.setState({
-                arrayShoeColor: [...this.state.arrayShoeColor,index.shoe_color],
-                save_name:index.shoe_name,
-                clickedName : 0
-              });
-            }else{
-              this.setState({
-                arrayShoeColor: [...this.state.arrayShoeColor,index.shoe_color],
-                save_name:index.shoe_name
-              });
-            }
-          }
-        })
-   })
-  }
-
-  handleToggle(event,clickedModel) {
+  /* search and submit -> model,name,color */  
+  handleModel(event) {
     let value = event.currentTarget.value
 
     this.setState({
@@ -134,12 +40,7 @@ export default class Search extends Component {
       arrayShoeColor:[],
       save_model:value,
       save_name:'',
-      save_color:'',
-      save_size:'',
-      clickedModel : 1,
-      clickedName:1,
-      clickedColor:1,
-      clickedSize:1
+      save_color:''
     });
 
     fetch('/information', {
@@ -151,100 +52,112 @@ export default class Search extends Component {
       .then(response => response.json())
       .then(result => {
 
-        result.map(index => {
-            var check_duplicate = 0
+      result.map(index => {
+         
+          if( index.shoe_model == value){//select model there is in DB(data base)
+            var check_duplicate = 0;
 
-            if( index.shoe_model == value){
-              if( clickedModel == 1){
-
-
-                this.state.arrayShoeName.map((item,ind) =>{
-                  if( item == index.shoe_name){
-                    check_duplicate = 1
-                  }else{
-                    check_duplicate = 0
-                  }
-                })
-
-                if(check_duplicate == 1){
-                  this.setState({
-                    save_model:index.shoe_model,
-                    clickedModel : 0
-                  });
-                }else{
-                  this.setState({
-                    arrayShoeName: [...this.state.arrayShoeName,index.shoe_name],
-                    save_model:index.shoe_model,
-                    clickedModel : 0
-                  });
-                }
-  
-              }else{
-
-                this.state.arrayShoeName.map((item,ind) =>{
-                  if( item == index.shoe_color){
-                    check_duplicate = 1
-                  }else{
-                    check_duplicate = 0
-                  }
-                })
-
-                if(check_duplicate == 1){
-                  this.setState({
-                    arrayShoeName: [...this.state.arrayShoeName,index.shoe_name],
-                    save_model:index.shoe_model
-                  });
-                }else{
-                  this.setState({
-                    arrayShoeName: [...this.state.arrayShoeName,index.shoe_name],
-                    save_model:index.shoe_model
-                  });
-                }
-
+            this.state.arrayShoeName.map((item,ind) =>{ // find duplicate name if there is
+              if( item == index.shoe_name){
+                check_duplicate = 1
+              }else if(check_duplicate != 1){
+                check_duplicate = 0
               }
-            }
-        })
+            })
+
+            if(check_duplicate == 1){//there is duplicate name for the same model so -> do not add it to arrayShoeName
+              this.setState({
+                save_model:index.shoe_model
+              });
+            }else{
+              this.setState({//add name to arrayShoeName
+                arrayShoeName: [...this.state.arrayShoeName,index.shoe_name],
+                save_model:index.shoe_model
+              });
+            }    
+          }
       })
+    })
+  }
+
+  handleName(event) {
+    let value = event.currentTarget.value;
 
     this.setState({
-      detail: 1
-    });
+    arrayShoeColor:[],
+    save_name:value,
+    save_color:''
+  });
 
- }
+  fetch('/information', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .then(result => {
+
+      result.map(index => {
+
+        //select name and chosen model there are in DB(data base) 
+        if( (index.shoe_name == value) && (index.shoe_model == this.state.save_model) ){
+          var check_duplicate = 0;
+
+          this.state.arrayShoeColor.map((item,ind) =>{ // find duplicate color if there is
+            if( item == index.shoe_color){
+              check_duplicate = 1
+            }else if(check_duplicate != 1){
+              check_duplicate = 0
+            }
+          })
+
+          if(check_duplicate == 1){//there is duplicate color for the same model and name so -> do not add it to arrayShoeColor
+            this.setState({
+              save_name:index.shoe_name
+            });
+          }else{
+              this.setState({
+                arrayShoeColor: [...this.state.arrayShoeColor,index.shoe_color],
+                save_name:index.shoe_name
+              });
+          }    
+        }
+
+      })
+    })
+  }
   
-  handleSubmitSize = async e => {
-    e.preventDefault();
-
-    const response = await fetch('/information', {
+  handleColor(event) {
+    let value = event.currentTarget.value
+    fetch('/information', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       }
     })
+      .then(response => response.json())
+      .then(result => {
 
-    const body = await response.json(); 
-    
-    body.map(index => {
-      index.shoe_size.map((item,index2) => {
-        if(this.state.searchSize == item){
-          
-          this.setState({
-            arraySize: [...this.state.arraySize,index.shoe_image],
-            array_code_size : [...this.state.array_code_size,index.shoe_code],
-            validationsize : 1
-          })
+        result.map(index => {
             
-        }
-      })
+          //select color and chosen name and chosen model there are in DB(data base) 
+          if((index.shoe_model == this.state.save_model) ){
+            if( (index.shoe_name == this.state.save_name) ){
+              if( (index.shoe_color == value)){
+                index.shoe_size.map((value) => {
+                  this.setState({
+                    save_color:index.shoe_color
+                  });
+                })
+              }
+            }
+          }
+          
+        })
     })
-
-    if(this.state.validationsize == 0){
-      alert("سایز کفش انتخاب نشده است");
-    }
+  }
   
-  };
-
-
   handleSubmit = async e => {
     e.preventDefault();
 
@@ -261,47 +174,47 @@ export default class Search extends Component {
     
     body.map(index => {
       
-      if(this.state.save_model != ''  && this.state.save_name != '' && this.state.save_color != '' ){
+      if(this.state.save_model != ''  && this.state.save_name != '' && this.state.save_color != '' ){ //user selectes model and name and color
         
         if(this.state.save_model == index.shoe_model  && this.state.save_name == index.shoe_name 
             && this.state.save_color == index.shoe_color ){
             
-                joined = this.state.array.concat(index.shoe_image);
+                joined = this.state.array_image.concat(index.shoe_image);
                 code = this.state.array_code.concat(index.shoe_code);
   
                 this.setState({
                   validation : 1,
-                  array: joined,
+                  array_image: joined,
                   array_code : code
                 })
             
           }
 
-        }else if(this.state.save_model != ''  && this.state.save_name != ''  ){
+        }else if(this.state.save_model != ''  && this.state.save_name != ''  ){ //user selectes model and name
 
           if(this.state.save_model == index.shoe_model  && this.state.save_name == index.shoe_name  ){
             
-              joined = this.state.array.concat(index.shoe_image);
+              joined = this.state.array_image.concat(index.shoe_image);
               code = this.state.array_code.concat(index.shoe_code);
 
               this.setState({
                 validation : 1,
-                array: joined,
+                array_image: joined,
                 array_code : code
               })
           
           }
 
-        }else if(this.state.save_model != ''  ){
+        }else if(this.state.save_model != ''  ){ //user selectes model
 
           if(this.state.save_model == index.shoe_model ){
         
-            joined = this.state.array.concat(index.shoe_image);
+            joined = this.state.array_image.concat(index.shoe_image);
             code = this.state.array_code.concat(index.shoe_code);
 
             this.setState({
               validation : 1,
-              array: joined,
+              array_image: joined,
               array_code : code
             })
       
@@ -312,13 +225,53 @@ export default class Search extends Component {
     })
       
     if(this.state.validation == 0){
-      alert("گزینه ای انتخاب نشده است");
+      alert("کفش موجود نیست");
     }
 
   };
   
+ /* search and submit -> size */
+  handelSearchSize(event) {
+    this.setState({
+      save_size: event.currentTarget.value
+    }); 
+  }
+
+  handleSubmitSize = async e => {
+    e.preventDefault();
+
+    const response = await fetch('/information', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    const body = await response.json(); 
+    
+    body.map(index => {
+      index.shoe_size.map((item,index2) => {
+        if(this.state.save_size == item){
+          
+          this.setState({
+            array_size: [...this.state.array_size,index.shoe_image],
+            array_code_size : [...this.state.array_code_size,index.shoe_code],
+            validationsize : 1
+          })
+            
+        }
+      })
+    })
+
+    if(this.state.validationsize == 0){
+      alert("سایز کفش موجود نیست");
+    }
+  
+  };
+
+
   render() {
-    var arraysize_1_to_100 = Array.from(Array(60).keys()) // 0 to 100
+    var arraysize_1_to_60 = Array.from(Array(60).keys()) // 1 to 60
     
     return (
       <div>
@@ -331,7 +284,8 @@ export default class Search extends Component {
                 
         <div id="wrap1">
 
-          {this.state.array.map((item, index) => (
+          {/*show shoe(s) after search model,name,color*/}
+          {this.state.array_image.map((item, index) => (
             <div id = "border_update" >
               <Link to={ `/shoe${this.state.array_code[index]}` }>
                 <img id="image" src={item}/>
@@ -340,7 +294,8 @@ export default class Search extends Component {
       
           ))}
 
-          {this.state.arraySize.map((item, index) => (
+           {/*show shoe(s) after search size*/}
+          {this.state.array_size.map((item, index) => (
             <div id = "border_update" >
               <Link to={ `/shoe${this.state.array_code_size[index]}` }>
                 <img id="image" src={item}/>
@@ -350,6 +305,7 @@ export default class Search extends Component {
 
         </div>
 
+         {/*search model,name,color*/}
         <div id="wrap3">
           <div id="wrap2">
             <DropdownSelectName 
@@ -357,7 +313,7 @@ export default class Search extends Component {
               lableName = "برند"
               placeholder = "برند کفش"
               val = {this.state.arraymodel}
-              _handleChange = {event => this.handleToggle(event,this.state.clickedModel)}
+              _handleChange = {event => this.handleModel(event)}
               />
 
             <DropdownSelectName 
@@ -365,7 +321,7 @@ export default class Search extends Component {
               lableName = "نام"
               placeholder = "نام کفش"
               val = {this.state.arrayShoeName}
-              _handleChange = {event => this.handleToggleName(event,this.state.clickedName) }
+              _handleChange = {event => this.handleName(event) }
               />
 
             <DropdownSelectName 
@@ -373,7 +329,7 @@ export default class Search extends Component {
               lableName = "رنگ"
               placeholder = "رنگ کفش"
               val = {this.state.arrayShoeColor}
-              _handleChange = {event => this.handleToggleColor(event,this.state.clickedColor) }
+              _handleChange = {event => this.handleColor(event) }
               />
           </div>
 
@@ -385,13 +341,14 @@ export default class Search extends Component {
 
         </div>
 
+         {/*search size*/}
         <div id="wrap4">
           <div id="wrap5">
             <DropdownSelect 
               name = "heoo"
               lableName = "numbers"
               placeholder = "سایز کفش"
-              val = {arraysize_1_to_100}
+              val = {arraysize_1_to_60}
               _handleChange = {event =>  this.handelSearchSize(event) }
               /> 
           </div>
