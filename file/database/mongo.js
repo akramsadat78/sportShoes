b59ec4de75f3a8e4dce2916612855c5c9860143e
unknown <safavimoghadam.89@gmail.com>
2021-06-01 11:52:25 +0430

    clean code

--- file/database/mongo.js
+++
@@ -1,7 +0,0 @@
-
-const MongoClient = require('mongodb').MongoClient;
-
-module.exports = {
-    DB: "mongodb+srv://akram:a96243040@cluster0.agvdo.mongodb.net/Shoe-sales-management?retryWrites=true&w=majority"
-
-}
